cbab39187b9219e6c10c02a7890114e7c3c986bf
Oscar <Osramirezf@gmail.com>
2021-09-01 00:12:08 -0400

    Se ha mejorado la interfaz de login

--- lib/login.dart
+++ lib/login.dart
@@ -1,3 +1,4 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 
 class Login extends StatefulWidget {
@@ -41,9 +42,81 @@ class _LoginState extends State<Login> with TickerProviderStateMixin {
 
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
       body: Stack(
-        children: [],
+        children: [
+          CachedNetworkImage(
+            imageUrl:
+                "https://images.pexels.com/photos/273238/pexels-photo-273238.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+            placeholder: (context, url) => Image.asset(
+              'assets/images/wallpaper.jpg',
+            ),
+            errorWidget: (context, url, error) => Icon(Icons.error),
+            width: double.infinity,
+            height: double.infinity,
+            fit: BoxFit.cover,
+            alignment: FractionalOffset(_animation.value, 0),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 16,
+            ),
+            child: ListView(
+              children: [
+                SizedBox(
+                  height: size.height * 0.1,
+                ),
+                Text(
+                  'Login',
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      color: Colors.grey.shade700,
+                      fontSize: 35),
+                ),
+                SizedBox(height: 10),
+                RichText(
+                  text: TextSpan(
+                    children: [
+                      TextSpan(
+                        text: 'No tienes cuenta ',
+                        style: TextStyle(
+                            color: Colors.grey.shade700,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16),
+                      ),
+                      TextSpan(text: '  '),
+                      TextSpan(
+                        text: 'Registrate! ',
+                        style: TextStyle(
+                            decoration: TextDecoration.underline,
+                            color: Colors.blue.shade500,
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16),
+                      ),
+                    ],
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                TextFormField(
+                  validator: (value) {
+                    if (value!.isEmpty || !value.contains('@')) {
+                      return 'Por favor ingrese un correo valido';
+                    } else {
+                      return null;
+                    }
+                  },
+                  style: TextStyle(color: Colors.white),
+                  decoration: InputDecoration(
+                      enabledBorder: UnderlineInputBorder(
+                          borderSide: BorderSide(color: Colors.white))),
+                )
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
